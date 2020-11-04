eb90b1eedfe4c05029a497775fb5d63b5423bb3e
Javier <javier19pedraza@gmail.com>
2020-11-04 11:28:25 -0600

    se agrego input y boton para agregar lineas en canvas

--- dibujo.js
+++ dibujo.js
@@ -1,36 +1,11 @@
 /* VARIABLES  */
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick );
 
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var yi, xf;
-var yin, xfn;
-
-var colorCeleste = "orange";
-var colorPink = "brown";
-
-
-/* CICLO */
-
-for( l = 0; l < lineas ; l++){
-
-    yi = 10 * l;
-    xf= 10 * (l + 1);
-
-    dibujarLinea(colorCeleste, 0, yi, xf, 299);
-    dibujarLinea(colorPink, 299, yi, xf, 0);
-    console.log("linea " + l)    
-
-}
-
-
-dibujarLinea(colorCeleste,1,1,1,299 );
-dibujarLinea(colorCeleste,1,299,299,299 );
-
-dibujarLinea(colorPink,299, 299, 299, 1 );
-dibujarLinea(colorPink,299, 1, 1, 1 );
-
-
 
 /* FUNCIONES */
 
@@ -42,7 +17,36 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
     lienzo.closePath();
-
 };
 
 
+function dibujoPorClick()
+{ 
+
+    var xxx= parseInt(texto.value);
+    var lineas = xxx;
+    var yi, xf;
+    var yin, xfn;
+    var colorOrange = "orange";
+    var colorPink = "brown";
+    var espacio = ancho/lineas;
+
+
+/* CICLO */
+
+for( l = 0; l < lineas ; l++)
+{
+
+yi = espacio * l;
+xf= espacio * (l + 1);
+
+    dibujarLinea(colorOrange, 0, yi, xf, 299);
+                dibujarLinea(colorPink, 299, yi, xf, 0);
+
+}
+    dibujarLinea(colorOrange,1,1,1,299 );
+    dibujarLinea(colorOrange,1,299,299,299 );
+
+                dibujarLinea(colorPink,299, 299, 299, 1 );
+                dibujarLinea(colorPink,299, 1, 1, 1 );
+} 
